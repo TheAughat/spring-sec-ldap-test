bfdbb671a584673408b865f7be6ae6cab957fe0c
Daniel Rodrigues <rcdan22@gmail.com>
2022-08-04 01:18:37 +0100

    initial commit

---
+++ src/main/java/com/example/springsecldaptest/SpringSecLdapTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.springsecldaptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecLdapTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecLdapTestApplication.class, args);
+	}
+
+}
+
+// using javaBrains tut with https://spring.io/guides/gs/authenticating-ldap/
+// https://stackoverflow.com/questions/72381114/spring-security-upgrading-the-deprecated-websecurityconfigureradapter-in-spring
---
+++ src/test/java/com/example/springsecldaptest/SpringSecLdapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecldaptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecLdapTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
